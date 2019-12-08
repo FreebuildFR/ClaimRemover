1b1920732c277ab4a5d87a7ad55c80d0b18fc82a
EpiCanard <epicanard@gmail.com>
2019-12-08 18:15:47 +0100

    Add delete command

--- src/main/scala/fr/freebuild/claimremover/commands/DeleteCommand.scala
+++ src/main/scala/fr/freebuild/claimremover/commands/DeleteCommand.scala
@@ -1,14 +1,42 @@
 package fr.freebuild.claimremover.commands
 
+import br.net.fabiozumbi12.RedProtect.Bukkit.{RedProtect, Region}
+import fr.freebuild.claimremover.ClaimRemoverPlugin.{analysis, configs}
+import fr.freebuild.claimremover.utils.PlayerUtils
 import org.bukkit.command.CommandSender
 
 object DeleteCommand extends Command {
 
   override def execute(sender: CommandSender, args: Seq[String]): Boolean = {
-    args.headOption match {
-      case Some("confirm") => println("Yes sure")
-      case _ => println("Are you sure ?")
+    if (analysis == null) {
+      PlayerUtils.sendMessage(sender, configs.language.errorMessages.cantExecuteCommand)
+      PlayerUtils.sendMessage(sender, configs.language.errorMessages.noAnalyze)
+      false
+    } else {
+      args.headOption match {
+        case Some("confirm") => removeClaims(sender, analysis.regions)
+        case _ => PlayerUtils.sendMessage(sender, configs.language.infoMessages.confirmDelete.format(analysis.regions.size))
+      }
+      true
     }
-    true
+  }
+
+  /**
+   * Remove all claims available inside analysis
+   *
+   * @param sender Player or console that executed the command
+   * @param regions Regions analyzed
+   */
+  private def removeClaims(sender: CommandSender, regions: List[Region]): Unit = {
+    PlayerUtils.sendMessage(sender, configs.language.infoMessages.claimsToDelete.format(regions.size))
+    val gap = Math.round(regions.size / 10)
+    regions.zipWithIndex.foreach {
+      case (region, index) => {
+        if ((index + 1) % gap == 0) {
+          PlayerUtils.sendMessage(sender, configs.language.infoMessages.claimsDeleted.format(index + 1, regions.size))
+        }
+        RedProtect.get().getAPI.removeRegion(region)
+      }
+    }
   }
 }
