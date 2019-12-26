4c049fe3d0d98aeeb36b5b4a61b4cdc4f9d4f706
EpiCanard <epicanard@gmail.com>
2019-12-26 23:21:15 +0100

    Fix claim removed when first connect

--- src/main/scala/fr/freebuild/claimremover/utils/PlayerUtils.scala
+++ src/main/scala/fr/freebuild/claimremover/utils/PlayerUtils.scala
@@ -10,7 +10,13 @@ import fr.freebuild.claimremover.utils.ColorUtils._
 object PlayerUtils {
   def getOfflinePlayer(uuid: String): OfflinePlayer = ClaimRemoverPlugin.getServer.getOfflinePlayer(UUID.fromString(uuid))
 
-  def getLastConnection(uuid: String): Date = new Date(getOfflinePlayer(uuid).getLastPlayed)
+  def getLastConnection(uuid: String): Date = {
+    val player = getOfflinePlayer(uuid)
+    if (player.isOnline)
+      new Date()
+    else
+      new Date(player.getLastPlayed)
+  }
 
   def sendMessage(sender: CommandSender, message: String): Unit = sender.sendMessage(message.toColor)
 }
