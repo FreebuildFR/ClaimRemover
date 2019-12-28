2d702c76a2da6c7438c7edf84fafb2fe3b42e62e
EpiCanard <epicanard@gmail.com>
2019-12-29 00:40:33 +0100

    Add permissions system and add usage

--- src/main/scala/fr/freebuild/claimremover/ClaimRemoverCommandExecutor.scala
+++ src/main/scala/fr/freebuild/claimremover/ClaimRemoverCommandExecutor.scala
@@ -3,6 +3,7 @@ package fr.freebuild.claimremover
 import java.util
 
 import fr.freebuild.claimremover.commands.{AnalyzeCommand, ClearCommand, ConfigCommand, DeleteCommand, ExportCommand, ImportCommand, InfoCommand, ReloadCommand, VersionCommand}
+import fr.freebuild.claimremover.Permissions
 import org.bukkit.command.{Command, CommandExecutor, CommandSender, TabCompleter}
 
 import scala.jdk.CollectionConverters._
@@ -11,6 +12,17 @@ import better.files._
 object ClaimRemoverCommandExecutor extends CommandExecutor with TabCompleter {
   private val mainCommands = Seq("analyze", "config", "clear", "export", "import", "info", "delete", "reload", "version")
   private val deleteSubCommands = Seq("confirm")
+  private val commands = Map(
+    "analyze" -> (Permissions.CMD_ANALYZE, AnalyzeCommand),
+    "export" -> (Permissions.CMD_EXPORT, ExportCommand),
+    "import" -> (Permissions.CMD_IMPORT, ImportCommand),
+    "delete" -> (Permissions.CMD_DELETE, DeleteCommand),
+    "reload" -> (Permissions.CMD_RELOAD, ReloadCommand),
+    "info" -> (Permissions.CMD_INFO, InfoCommand),
+    "config" -> (Permissions.CMD_CONFIG, ConfigCommand),
+    "clear" -> (Permissions.CMD_CLEAR, ClearCommand),
+    "version" -> (Permissions.CMD_VERSION, VersionCommand)
+  )
 
   /**
    * Called when ClaimRemover command is executed
@@ -23,21 +35,11 @@ object ClaimRemoverCommandExecutor extends CommandExecutor with TabCompleter {
    */
     override def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
       args.toSeq match {
-        case head +: tail => head match {
-          case "analyze" => {
-            AnalyzeCommand.execute(sender, tail)
-            InfoCommand.execute(sender, tail)
-          }
-          case "export" => ExportCommand.execute(sender, tail)
-          case "import" => ImportCommand.execute(sender, tail)
-          case "delete" => DeleteCommand.execute(sender, tail)
-          case "reload" => ReloadCommand.execute(sender, tail)
-          case "info" => InfoCommand.execute(sender, tail)
-          case "clear" => ClearCommand.execute(sender, tail)
-          case "config" => ConfigCommand.execute(sender, tail)
-          case "version" => VersionCommand.execute(sender, tail)
-          case _ => false
-        }
+        case head +: tail =>
+          commands.get(head).exists(command => {
+            val (permission, executor) = command
+            if (permission.isSetOnWithMessage(sender)) executor.execute(sender, tail) else true
+          })
         case _ => false
       }
     }
@@ -53,7 +55,7 @@ object ClaimRemoverCommandExecutor extends CommandExecutor with TabCompleter {
    */
   override def onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array[String]): util.List[String] = {
     (args.toSeq match {
-      case head +: Nil => filterCommands(mainCommands, head)
+      case head +: Nil => filterCommands(commands, head, sender)
       case "delete" +: tail => tail match {
         case deleteHead +: Nil => filterCommands(deleteSubCommands, deleteHead)
         case _ => Seq.empty
@@ -74,6 +76,19 @@ object ClaimRemoverCommandExecutor extends CommandExecutor with TabCompleter {
    * @return Sequence filtered
    */
   private def filterCommands(commands: Seq[String], search: String) = commands.filter(_.startsWith(search))
+
+  /**
+   * Filter a Seq of main commands with startwith search
+   *
+   * @param commands List of commands
+   * @param search Search sentence
+   * @param sender Command executor
+   * @return Sequence filtered
+   */
+  private def filterCommands(commands: Map[String, (Permission, fr.freebuild.claimremover.commands.Command)], search: String, sender: CommandSender) = commands.filter(cmd => {
+    val (name, command) = cmd
+    name.startsWith(search) && command._1.isSetOn(sender)
+  }).keys
 
   /**
    * Filter analysis directory inside exports directory
---
+++ src/main/scala/fr/freebuild/claimremover/Permissions.scala
@@ -0,0 +1,27 @@
+package fr.freebuild.claimremover
+
+import fr.freebuild.claimremover.utils.PlayerUtils
+import fr.freebuild.claimremover.ClaimRemoverPlugin.configs
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+case class Permission(value: String) {
+  def isSetOn(sender: CommandSender): Boolean = sender.hasPermission(value)
+  def isSetOnWithMessage(sender: CommandSender): Boolean = {
+    val isSet = isSetOn(sender)
+    if (!isSet) PlayerUtils.sendMessage(sender, configs.language.errorMessages.permissionNotAllowed)
+    isSet
+  }
+}
+
+object Permissions {
+  val CMD_ANALYZE = Permission("claimremover.commands.analyze")
+  val CMD_CLEAR = Permission("claimremover.commands.clear")
+  val CMD_CONFIG = Permission("claimremover.commands.config")
+  val CMD_DELETE = Permission("claimremover.commands.delete")
+  val CMD_EXPORT = Permission("claimremover.commands.export")
+  val CMD_IMPORT = Permission("claimremover.commands.import")
+  val CMD_INFO = Permission("claimremover.commands.info")
+  val CMD_RELOAD = Permission("claimremover.commands.reload")
+  val CMD_VERSION = Permission("claimremover.commands.version")
+}
